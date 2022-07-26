8a92c9c7ebeb385d77f2475b6aa6a180621e3b52
Elena Denezhnaia <elenamelicenta@gmail.com>
2022-07-26 16:59:44 +0300

    express server added

--- server.js
+++ server.js
@@ -5,12 +5,17 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/recurrence'));
+ app.use(express.static(__dirname));
+ app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function(req,res) {
+ app.get('/ping', function (req, res) {
+  return res.send('pong');
+ });
 
-  res.sendFile(__dirname +'/index.html');
-});
+
+ app.get('/*', function (req, res) {
+ res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ });
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT ||  3000);
